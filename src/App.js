614a9e31a34bade5aa03f8837971e87f6e11315a
Mario <maliqimario@gmail.com>
2022-06-20 21:30:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Form from "react-bootstrap/Form"
+import React, { useState } from "react";
+
 
 function App() {
+
+  const [temp, setTemp] = useState("");
+  const [location, setLocation] = useState("");
+
+  const formSubmitted = (event) => {
+    event.preventDefault();
+    fetch(`http://api.weatherapi.com/v1/current.json?key=c621479ed6a34ab0baa162013222006&q=${location}&aqi=yes`)
+      .then(response => response.json())
+      .then(data => {
+        setTemp(data.current.temp_c)
+        console.log(data);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather App</h1>
+      <Form onSubmit={formSubmitted}>
+        <Form.Label>Location</Form.Label>
+        <Form.Control value={location} type="location" placeholder="Enter Location" onChange={event => {
+          setLocation(event.target.value);
+        }} />
+        <Form.Text className="text-muted">
+          preferably a city :)
+        </Form.Text>
+      </Form>
+      {temp && (
+        <h1>{temp}°</h1>
+      )}
     </div>
   );
 }
