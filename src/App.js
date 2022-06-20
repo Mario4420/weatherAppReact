d753ef0edc3ae6e926be93f813f7318aa40a08d3
Mario <maliqimario@gmail.com>
2022-06-20 21:43:06 +0200

    hid api key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import Form from "react-bootstrap/Form"
 import React, { useState } from "react";
 
-
 function App() {
 
   const [temp, setTemp] = useState("");
@@ -9,7 +8,7 @@ function App() {
 
   const formSubmitted = (event) => {
     event.preventDefault();
-    fetch(`http://api.weatherapi.com/v1/current.json?key=c621479ed6a34ab0baa162013222006&q=${location}&aqi=yes`)
+    fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.REACT_APP_API_KEY}&q=${location}&aqi=yes`)
       .then(response => response.json())
       .then(data => {
         setTemp(data.current.temp_c)
